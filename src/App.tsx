c2b0fddd839f7e57def85aad278e9ae607fd9679
sk0ik <koikesyouhei1003@gmail.com>
2026-05-24 16:07:43 +0900

    chore: Move default menu position further down

--- src/App.tsx
+++ src/App.tsx
@@ -492,7 +492,7 @@ function CustomUI({ editor }: { editor: any }) {
 
   const [isOpen, setIsOpen] = useState(false)
   const [isHorizontal, setIsHorizontal] = useState(false)
-  const [pos, setPos] = useState({ x: 10, y: 120 })
+  const [pos, setPos] = useState({ x: 10, y: 250 })
   const [isDragging, setIsDragging] = useState(false)
   const [dragOffset, setDragOffset] = useState({ x: 0, y: 0 })
 
